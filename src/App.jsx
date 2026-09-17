3256ccaf9b448f3cbee93d5e78de163f41f9adc3
CariBosio <caribosio72@gmail.com>
2026-09-16 23:55:30 -0300

    chore: initial project scaffolding and base cleanup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>QualityTrack</h1>
+      <p>Scaffolding inicial listo para migración.</p>
+    </div>
+  );
+}
